08173b6c73eddc4c65af82050a001f0513c6db92
Osman Said <57035358+Umid0vic@users.noreply.github.com>
2024-05-22 19:16:52 +0200

    Added Email class

---
+++ Models/Email.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomerRegistry.Models;
+
+public class Email
+{
+    public string BusinessEmail { get; private set; }
+    public string PrivateEmail { get; private set; }
+
+    public Email(string businessEmail, string privateEmail)
+    {
+        this.BusinessEmail = businessEmail;
+        this.PrivateEmail = privateEmail;
+    }
+
+    public override string ToString()
+    {
+        return $"Business: {BusinessEmail}, Private: {PrivateEmail}";
+    }
+}

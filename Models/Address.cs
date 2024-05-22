1596858cdddc2c54917c905023e3373a86275b1f
Osman Said <57035358+Umid0vic@users.noreply.github.com>
2024-05-22 19:13:04 +0200

    Added Address class

---
+++ Models/Address.cs
@@ -0,0 +1,32 @@
+﻿namespace CustomerRegistry.Models;
+
+public class Address
+{
+    public string Street { get; private set; }
+    public string City { get; private set; }
+    public string ZipCode { get; private set; }
+    public string Country { get; private set; }
+
+    public Address(string street, string city, string zipCode, string country)
+    {
+        this.Street = street;
+        this.City = city;
+        this.ZipCode = zipCode;
+        this.Country = country;
+    }
+
+    public Address(string city, string zipCode, string country)
+        : this("", city, zipCode, country)
+    {
+    }
+
+    public Address(string country)
+        : this("", "", "", country)
+    {
+    }
+
+    public override string ToString()
+    {
+        return $"{Street}, {City}, {ZipCode}, {Country}";
+    }
+}

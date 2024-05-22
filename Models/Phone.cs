47e4dc5c27d813dfab614551a60e78f27fb12a0b
Osman Said <57035358+Umid0vic@users.noreply.github.com>
2024-05-22 19:19:14 +0200

    Added Phone class

---
+++ Models/Phone.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomerRegistry.Models;
+
+public class Phone
+{
+    public string HomePhone { get; private set; }
+    public string CellPhone { get; private set; }
+
+    public Phone(string phone, string cellPhone)
+    {
+        this.HomePhone = phone;
+        this.CellPhone = cellPhone;
+    }
+
+    public override string ToString()
+    {
+        return $"Home: {HomePhone}, Cell: {CellPhone}";
+    }
+}
